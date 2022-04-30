37bb32997cb5e76eef974a7f1d3bcf173509fd01
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-04-30 19:17:13 +0100

    Setup plot post for publishing

--- plot.py
+++ plot.py
@@ -32,18 +32,18 @@ from matplotlib.offsetbox import AnchoredText
 #  - The *threads.csv* file and,
 #  - The *comments/comments_[THREAD_ID].csv* files
 #
-# The first group is a single file that contains some high-level information that acts as an aggregator for the rest of the files. In this file one can find the name, author, title, creation date, score and number of comments of each one of the *live threads* related to the invasion.
+# The first group is a single file that contains some high-level information that acts as an aggregator for the rest of the files. In this file, one can find the name, author, title, creation date, score and number of comments of each one of the *live threads* related to the invasion.
 
 # %%
 threads = pd.read_csv("data/threads.csv")
 threads.head(2)
 
 # %% [markdown]
-# The second group contains as many files as threads exists in the *threads.csv* file, each one of them has a name like *comments/comments_[THREAD_ID].csv*.
+# The second group contains as many files as threads exist in the *threads.csv* file; each has a name like *comments/comments_[THREAD_ID].csv*.
 #
-# Each row in these *csv* files represents a comment made in the parent thread, the information available for each comment is: author, identifier, body, created date/time, whether it has been edited, score, and the parent comment (if it is a reply).
+# Each row in these *csv* files represents a comment made in the parent thread. The information available for each comment is: author, identifier, body, created date/time, whether it has been edited, score, and the parent comment (if it is a reply).
 #
-# One thing to note though, is that one can not simply use `pd.read_csv`, since sometimes the comments may contain line breaks that make it so that sometimes a single comment uses more than one row in the file. To succesfully read all these files, one needs to pass the `lineterminator` argument:
+# One thing to note is that one can not simply use `pd.read_csv`, since sometimes the comments may contain line breaks that make it so that sometimes a single comment uses more than one row in the file. To successfully read all these files, one needs to pass the `lineterminator` argument:
 
 # %%
 file = "data/comments/comments__st8lq0.csv"
@@ -53,19 +53,19 @@ comments.head(2)
 # %% [markdown]
 # ## Plotting the frequency of comments
 #
-# Now that we learned what the files contain and how to read them, let's do something cool with them. Let's see how the interest over the thread has changed over time by counting the number of comments per hour.
+# Now that we learned what the files contain and how to read them, let's do something cool with them. Let's see how the interest in the thread has changed over time by counting the number of comments per hour.
 #
 # The overall process is as follows:
 #
 #  1. Read all the comment's dates
-#  2. Bin the dates by 1 hour intervals
+#  2. Bin the dates by 1-hour intervals
 #  3. Plot!
 #
 
 # %% [markdown]
 # ### 1. Read all the comment's dates
 #
-# We can keep using pandas, but being more clever about how to use it. Did you know that you can specify that you only want a subset of columns with the `usecols` argument?
+# We can keep using *pandas* but being more clever about using it. Did you know that you can specify that you only want a subset of columns with the `usecols` argument?
 
 # %%
 created_dates = []
@@ -77,12 +77,12 @@ for thread_id in threads["id"]:
 created_dates = np.concatenate(created_dates)
 
 # %% [markdown]
-# This leaves us with the numpy array `created_dates` that contains $2,083,085$ numbers representing the creation date of each comment. The next step is binning these times into 1 hour intervals.
+# This leaves us with the NumPy array `created_dates` containing $2,083,085$ numbers representing the creation date of each comment. The next step is binning these times into 1 hour intervals.
 
 # %% [markdown]
 # ### 2. Binning the creation times
 #
-# We will make use of a couple of helper functions to round date times up or down to the nearest step in the interval we define, (and one more to visualise timestamps).
+# We will use a couple of helper functions to round date times up or down to the nearest step in the interval we define (and one more to visualise timestamps).
 
 # %%
 # Helper date functions
@@ -113,7 +113,7 @@ lower = humanise(lower_bound(example_ts))
 print(f"{lower} is the lower bound of {actual_date} and its upper bound is {upper}")
 
 # %% [markdown]
-# Now that we have a way to calculate the upper and lower bounds of a specific date, we can move on to calculate the bin edges. This is easy once we know what are the minimum and maximum dates in our `created_dates`. In fact, getting the bin edges is a one-liner with NumPy:
+# Now that we have a way to calculate the upper and lower bounds of a specific date, we can move on to calculate the bin edges. This is easy once we know the minimum and maximum dates in our `created_dates`. In fact, getting the bin edges is a one-liner with NumPy:
 
 # %%
 bin_edges = np.arange(
@@ -124,19 +124,19 @@ bin_edges = np.arange(
 )
 
 # %% [markdown]
-# Did I say one-liner? 😅 – well, I wanted it to be more understandable. The clever part comes when we add 1 to the upper bound, since `np.arange` is exclusive on the right-hand side, which means our valid upper bound would not be returned, however we circumvent this limitation by making it seem like our upper bound is not the last number. Lastly, the `step` argument has to be equal to 1 hour.
+# Did I say one-liner? 😅 – well, I wanted it to be more understandable. The clever part comes when we add 1 to the upper bound; since `np.arange` is exclusive on the right-hand side, which means our valid upper bound would not be returned, however, we circumvent this limitation by making it seem like our upper bound is not the last number. Lastly, the `step` argument has to be equal to 1 hour.
 #
-# Now that we have the bin edges, we are ready to calculate the histogram, this is yet another one liner thanks to NumPy's `np.histogram`!
+# Now that we have the bin edges, we are ready to calculate the histogram. This is yet another one-liner, thanks to NumPy's `np.histogram`!
 
 # %%
 values, bin_edges = np.histogram(created_dates, bins=bin_edges)
 
 # %% [markdown]
-# The values returned by this function are: the count for the specified interval and the intervals themselves. Keep in mind that there will always be one more item in the intervals than in the values!
+# The values returned by this function are the count for the specified interval and the intervals themselves. Keep in mind that there will always be one more item in the intervals than in the values!
 #
 # #### A window?
 #
-# Something that we can (and will do) is specify a determined window of time we want to show just in case we want to "zoom in" in our plot. For now, since the live threads started on the 14th of february 2022 we will take that as the beginning of our window and as for the end let's take the maximum date available + 1 day.
+# For further customisation purposes we can specify a determined window of time we want to show just in case we want to "zoom in" on our plot. For now, since the live threads started on the 14th of february 2022, we will take that as the beginning of our window and as for the end, let's take the maximum date available + 1 day.
 
 # %%
 begining = datetime(2022, 2, 14)
@@ -154,7 +154,7 @@ comments_histogram = pd.Series(data=values, index=pd.to_datetime(bin_edges[:-1],
 # %% [markdown]
 # #### Important events
 #
-# The plot on its own is informative, however, we can make it even more insightful with some important events about the invasion – this will help our users in assesing how a particular event in real life translates into a spike (or not) in the comments online.
+# The plot is informative; however, we can make it even more insightful with some important events about the invasion – this will help our users assess how a particular event in real life translates into a spike (or not) in the comments online.
 #
 # We need to create an array of tuples, where each tuple is the date of when the event happened and a short description of it:
 
@@ -206,20 +206,20 @@ line_plot()
 # %% [markdown]
 # ![First version](https://ik.imagekit.io/thatcsharpguy/posts/worldnews/first-version.png?ik-sdk-version=javascript-1.4.3&updatedAt=1651313288116)
 #
-# Not bad, but can be improved further!
+# Not bad, but it can be improved further!
 
 # %% [markdown]
 # #### Improving our ticks with locators and formatters
 #
 # The first thing I'd like to address is the fact that the visual references in terms of days and comment count look very sparse. Given that these are daily observations, I find it may be helpful to show this information on the graph.
 #
-# Turns out, *matplotlib* has some great utilities we can employ when working with dates within the `matplotlib.dates` package.
+# It turns out, that *matplotlib* has some great utilities we can employ when working with dates within the `matplotlib.dates` package.
 #
-# The function `add_ticks` is divided in 4 blocks:
+# The function `add_ticks` is divided into 4 blocks:
 #
 #  1. Set the minor ticks in the X-axis, using `DayLocator` and `DateFormatter` to set a minor marker every day
 #  2. Set the major ticks in the X-axis, using a `MonthLocator` and `DateForamtter` to set a major marker every month
-#  3. Set the formatting in the Y-axis, this one is a bit more convoluted since we need to "manually" set the ticks reading the original ones with `FixedLocator`, then use a `FuncFormatter` (and a lambda function) to specify the new formatting.
+#  3. Set the formatting in the Y-axis; this one is a bit more convoluted since we need to "manually" set the ticks reading the original ones with `FixedLocator`, then use a `FuncFormatter` (and a lambda function) to specify the new formatting.
 #  4. Set some additional styles to make the major ticks stand out from the minor ones.
 
 # %%
@@ -255,10 +255,10 @@ add_ticks(ax)
 # %% [markdown]
 # #### Where are my labels??
 #
-# An unlabeled plot is a crappy plot, guided by this principle let's add the `add_legends` function divided in two blocks:
+# An unlabeled plot is a crappy plot; guided by this principle, let's add the `add_legends` function divided into two blocks:
 #
-#  - It sets the limits of what the plot shows, here is were we use the `window` defined above, and also set the starting point in the Y-axis to 0.
-#  - It sets all the labels and credit to the plot
+#  - It sets the limits of what the plot shows; here is where we use the `window` defined above and set the Y-axis's starting point to 0.
+#  - It sets all the labels and credit to the plot.
 
 # %%
 def add_legends(axes, window):
@@ -286,7 +286,7 @@ add_legends(ax, window)
 # %% [markdown]
 # #### Major events
 #
-# Remember we created an array of tuples called `major_events`? it is its time to shine. The function `add_highlighted_events` takes the axis and the major events array and iterates over them marking their locations with the `.annotate` method.
+# Remember we created an array of tuples called `major_events`? Tt is its time to shine. The function `add_highlighted_events` takes the axis and the major events array and iterates over them, marking their locations with the `.annotate` method.
 
 # %%
 def add_highlighted_events(axes, events):
@@ -317,9 +317,9 @@ add_highlighted_events(ax, major_events)
 # ![Fourth version](https://ik.imagekit.io/thatcsharpguy/posts/worldnews/fourth-version.png?ik-sdk-version=javascript-1.4.3&updatedAt=1651313288116)
 
 # %% [markdown]
-# #### More colors
+# #### More colours
 #
-# In the `add_final_touches` you will find that I am adding a `grid` so that there is a subtle distinction across days. Set the background color of the plot to a light yellow and the overall background of our graphic to white.
+# In the `add_final_touches` function, you will find that I am adding a `grid` so that there is a subtle distinction across days. Set the background colour of the plot to a light yellow and the overall background of our graphic to white.
 
 # %%
 def add_final_touches(figure, axes):
@@ -345,13 +345,13 @@ add_final_touches(fig, ax)
 fig.savefig("worldnews.png")
 
 # %% [markdown]
-# #### Appendix: Missing data on the 26 of february
+# #### Appendix: Missing data on the 26 of February
 #
-# Do you see the drop between february 26 and 27? let's see what happened:
+# Do you see the drop between February 26 and 27? let's see what happened:
 #
 # The latest comment on the [Day 3, Part 6 (Thread #35)](https://reddit.com/r/worldnews/comments/t1oqrc/rworldnews_live_thread_russian_invasion_of/) thread was posted at 06:54:34 AM, while the earliest comment on the replacement thread, [Day 3, Part 7 (Thread #36)](https://www.reddit.com/r/worldnews/comments/t1rnuj/rworldnews_live_thread_russian_invasion_of/?sort=old) is 07:57:52 AM.
 #
-# This makes it seem like there were absolutely no comments for around one hour. However, upon further investigation, it seems that there was an error on the mods team where one of them created a thread with the wrong name, left it around for around 1 hour and then deleted it, as evidenced by these comments:
+# This makes it seem like there were absolutely no comments for around one hour. However, upon further investigation, it appears that there was an error on the mods team where one of them created a thread with the wrong name, left it around for around 1 hour and then deleted it, as evidenced by these comments:
 #
 #  > What happened to the last thread? – [*permalink*](https://www.reddit.com/r/worldnews/comments/t1rnuj/rworldnews_live_thread_russian_invasion_of/hyhpo2p/)
 #  >> Had the dates wrong, said it was day 4 thread 1.
@@ -361,7 +361,7 @@ fig.savefig("worldnews.png")
 #  > New thread already? – [*permalink*](https://www.reddit.com/r/worldnews/comments/t1rnuj/rworldnews_live_thread_russian_invasion_of/hyhpn5g/?utm_source=reddit&utm_medium=web2x&context=3)
 #  >> Wrong day on the last one
 #
-# Let's add another note to our plot so that people does not get confused:
+# Let's add another note to our plot so that people do not get confused:
 #
 
 # %%
@@ -390,8 +390,8 @@ fig.savefig("worldnews.png")
 # %% [markdown]
 # ## Conclusion
 #
-# And there we are, we have a plot that is even more interesting to look at (and it was fun to make too).
+# And there we are; we have a plot that is even more interesting to look at (and it was fun to make too).
 #
-# In the previous post we had a look into how to create a dataset using Reddit data and in this one we saw how to use this dataset to create something interesting, I hopw you learned something new or at least that you liked it. As always, code is available here and I am available to answer any question on [Twitter at @io_exception](https://twitter.com/io_exception).
+# In a previous post, we had a look into how to create a dataset using Reddit data, and in this one we saw how to use this dataset to create something you can present. I hope you learned something new or at least that you liked it. As always, [code is available here](https://github.com/fferegrino/r-worldnews-live-threads-ukraine/blob/main/plot.ipynb), and I am open to answering any question on [Twitter at @io_exception](https://twitter.com/io_exception).
 
 # %%
