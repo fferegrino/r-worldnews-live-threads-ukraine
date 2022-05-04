286c92346cc1a975c779d3234ec45c0a69ec52b7
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-05-04 12:08:06 +0100

    wip

--- from_jupyter/convert.py
+++ from_jupyter/convert.py
@@ -1,12 +1,14 @@
-import jupytext
-import imgkit
 import base64
-from pathlib import Path
-from nbformat.notebooknode import from_dict
+import logging
 import random
 import string
+from pathlib import Path
 
-from nbconvert.preprocessors import ExecutePreprocessor
+import imgkit
+import jupytext
+from nbformat.notebooknode import from_dict
+
+logger = logging.getLogger("from-jupyter")
 
 
 def make_markdown_cell(content):
@@ -22,45 +24,21 @@ def make_markdown_cell(content):
 
 def convert_to_md(file, execute=False, remove_code=False, save_resources=True):
     file = Path(file)
+    parent_dir = Path("exported", file.stem)
+    parent_dir.mkdir(exist_ok=True, parents=True)
+
     with open(file) as w:
         notebook = jupytext.read(w)
 
-        if jupytext_meta := notebook["metadata"].get("jupytext"):
-            execute = (jupytext_meta["text_representation"]["extension"] != "ipynb") and save_resources
+        # if jupytext_meta := notebook["metadata"].get("jupytext"):
+        #     execute = (jupytext_meta["text_representation"]["extension"] != "ipynb") and save_resources
 
-    image_count = 0
-    table_count = 0
+        # if execute:
+        #     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+        #     notebook, metadata = ep.preprocess(notebook, {"metadata": {"path": "."}})
 
     if save_resources:
-        parent_dir = Path(file.stem)
-        parent_dir.mkdir(exist_ok=True)
-
-        new_cells = []
-        if execute:
-            ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-            notebook, metadata = ep.preprocess(notebook, {"metadata": {"path": "."}})
-
-        for cell in notebook["cells"]:
-            new_cells.append(cell)
-            if outputs := cell.get("outputs", []):
-                if data := outputs[0].get("data", {}):
-                    metadata = cell.get("metadata")
-                    if "image/png" in data:
-                        new_cell = process_image_cell(data, image_count, metadata, parent_dir)
-                        new_cells.append(new_cell)
-                        image_count += 1
-                    elif "text/html" in data:
-
-                        image_name = metadata.get("file", f"table-{table_count}.jpg")
-                        options = {"zoom": 2, "quality": 100}
-                        with open("from_jupyter/dataframe.html") as r:
-                            template = r.read()
-                            imgkit.from_string(
-                                template.replace("{{DATAFRAME}}", data["text/html"]),
-                                parent_dir / image_name,
-                                options=options,
-                            )
-                            table_count += 1
+        new_cells = save_cell_resources(parent_dir, notebook)
         notebook["cells"] = new_cells
 
     if remove_code:
@@ -69,8 +47,31 @@ def convert_to_md(file, execute=False, remove_code=False, save_resources=True):
     jupytext.write(notebook, f"{file.stem}-exported.md")
 
 
-def process_image_cell(data, image_count, metadata, parent_dir):
-    image_name = metadata.get("file", f"image-{image_count}.png")
+def save_cell_resources(save_dir, notebook):
+    new_cells = []
+    for cell in notebook["cells"]:
+        new_cells.append(cell)
+        if outputs := cell.get("outputs", []):
+            if data := outputs[0].get("data", {}):
+                metadata = cell.get("metadata")
+                if "image/png" in data:
+                    new_cell = process_image_cell(data, metadata, save_dir)
+                    new_cells.append(new_cell)
+                elif "text/html" in data:
+                    image_name = metadata["file"]
+                    options = {"zoom": 2, "quality": 100}
+                    with open("from_jupyter/dataframe.html") as r:
+                        template = r.read()
+                        imgkit.from_string(
+                            template.replace("{{DATAFRAME}}", data["text/html"]),
+                            save_dir / image_name,
+                            options=options,
+                        )
+    return new_cells
+
+
+def process_image_cell(data, metadata, parent_dir):
+    image_name = metadata["file"]
     print(image_name)
     description = metadata.get("description", image_name)
     image_path = parent_dir / image_name
